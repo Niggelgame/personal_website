f3023951f78efd734736d348d2c2debcc3cb9d4b
Niggelgame <Niggelgame@users.noreply.github.com>
2021-03-28 09:14:03 +0000

    deploy: 81bbe0b370f165876dd6d117bb7a93fcb0b90250

--- _nuxt/static/1616663052/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616663052",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c8dd5540"}}}}(null,"\u002F"));
